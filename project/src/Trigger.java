496fcec5cfe9492e87799e0dbe8088f383524df0
Mark Nijboer <marknijboer8@gmail.com>
2015-09-23 11:35:30 +0200

    Added server

--- project/src/Trigger.java
+++ project/src/Trigger.java
@@ -1,6 +1,30 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 
 public class Trigger {
 	public static void main(String[] args) {
-		
+		try {
+			ServerSocket server = new ServerSocket(7789);
+			
+			while (true) {
+				Socket clientSocket = server.accept();
+				BufferedReader in = new BufferedReader (new InputStreamReader (clientSocket.getInputStream ()));
+				PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+
+			    String incommingText = "";
+			    try
+			    {
+			        incommingText = in.readLine ();
+			        System.out.println(incommingText);
+			    } catch(Exception e) {}
+			}
+		} catch(Exception e) {
+			System.out.println(e.getMessage());
+		}
+
 	}
 }
